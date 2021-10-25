d0b34e51e4448328c11e85d0e02324e03124b3d9
Alex-Vardanyan <88556305+Alex-Vardanyan@users.noreply.github.com>
2021-10-25 13:38:32 +0400

    Add files via upload

---
+++ CustomLinqExtension/MyLinq.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomLinq
+{
+    static class MyLinqExt
+    {
+        public static IEnumerable<TResult> SelectExt<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource,TResult> selector)
+        {
+            var result = new List<TResult>();
+            foreach(TSource item in source)
+            {
+                result.Add(selector(item));
+            }
+            return result;
+        }
+
+        public static IEnumerable<TSource> WhereExt<TSource>(this IEnumerable<TSource> source, Func<TSource,bool> predicate)
+        {
+            var result = new List<TSource>();
+            foreach (TSource item in source)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        public static IEnumerable<IGrouping<TKey,TSource>> GroupByExt<TSource,TKey>(this IEnumerable<TSource> source, Func<TSource,TKey> keySelector)
+        {
+            var result = new Dictionary<TKey, TSource>();
+            foreach (TSource item in source)
+            {
+                result.TryAdd(keySelector(item), item);
+            }
+            return result.Values as IEnumerable<IGrouping<TKey,TSource>>;
+        }
+
+        public static List<TSource> ToListExt<TSource>(this IEnumerable<TSource> source)
+        {
+            var result = new List<TSource>();
+            foreach (var item in source)
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+
+        public static IOrderedEnumerable<TSource> OrderByExt<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var result = new SortedList<TKey, TSource>();
+            foreach(TSource item in source)
+            {
+                result.TryAdd(keySelector(item), item);
+            }
+            return result.Values as IOrderedEnumerable<TSource>;
+        }
+
+        public static Dictionary<TKey, TSource> ToDictionaryExt<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource,TKey> keySelector)
+        {
+            var result = new Dictionary<TKey, TSource>();
+            foreach (TSource item in source)
+            {
+                var key = keySelector(item);
+                result.Add(key, item);
+            }
+            return result;
+        }
+    }
+}
---
+++ CustomLinqExtension/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using CustomLinq;
+using System.Collections.Generic;
+
+namespace CustomLinqExtensionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Demo
+            var list = new List<int> {0, 1, 2, 3, 4, 5};
+            var evenList = list.SelectExt(x => x * 2);
+            foreach (var item in evenList)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+
+            var AncientEmpiresAndPolices = new List<string> { "Rome", "Etruscan League", "Arvernii", "Boiotian League", "Athenai", "Seleucid Empire", "Carthage" };
+            var selection = AncientEmpiresAndPolices.WhereExt(x => x.Length > 7);
+            foreach (var item in selection)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+
+            var PopulationByCountry = new List<Country>();
+            var China = new Country() { Name = "China", Population = 1400000000, Capital = "Beijing" };
+            var India = new Country() { Name = "India", Population = 1380000000, Capital = "Delhi" };
+            var Japan = new Country() { Name = "Japan", Population = 12580000, Capital = "Tokyo" };
+            PopulationByCountry.Add(China);
+            PopulationByCountry.Add(India);
+            PopulationByCountry.Add(Japan);
+            var countryDict = PopulationByCountry.ToDictionaryExt(x => x.Name);
+            foreach (var item in countryDict)
+            {
+                Console.Write(item.Key + "-" + item.Value + ", ");
+            }
+            Console.WriteLine();
+
+            var orderedByPopulation = PopulationByCountry.OrderByExt(x => x.Population);
+            foreach (var item in orderedByPopulation)
+            {
+                Console.WriteLine(item.Name + " "+ item);
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+            #endregion
+
+        }
+        
+    }
+    class Country : System.Collections.IEnumerable
+    {
+        public string Name;
+        public int Population;
+        public string Capital;
+
+        public System.Collections.IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return Capital+" "+Population.ToString();
+        }
+    }
+
+}
